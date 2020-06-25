7d2114e9c4a9f93ff2a40e4847103210e56d441a
stamas01 <stamas01@gmail.com>
2020-06-25 20:07:00 +0100

    added requiremens.txt and updated example code

--- rotating_proxy/rotating_proxy.py
+++ rotating_proxy/rotating_proxy.py
@@ -27,9 +27,11 @@ class RotatingProxy:
         >>> from rotating_proxy.rotating_proxy import RotatingProxy
         >>>
         >>> url = "http(s)://YOURURL.COM"
+        >>> rp_http = RotatingProxy(https = False)
+        >>> rp_https = RotatingProxy(https = True)
         >>> while True:
-        >>>     rp_http = RotatingProxy(https = False)
-        >>>     rp_https = RotatingProxy(https = True)
+        >>>     rp_http.update()
+        >>>     rp_https.update()
         >>>     for proxy_http , proxy_https  in zip(rp_http, rp_https):
         >>>         proxyDict = {"http"  : "{0}:{1}".format(*proxy_http[0:2]),
         >>>                      "https"  : "{0}:{1}".format(*proxy_https[0:2])}
